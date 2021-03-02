876d706fd59c37ccb5ac0d6c4c7823699da47529
Christoph Steltner <mail@nplhse.com>
2021-03-02 23:20:37 +0100

    feat: Added welcome email.

---
+++ src/Message/NewUserMessage.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Message;
+
+use App\Entity\Hospital;
+use App\Entity\Import;
+use App\Entity\User;
+
+final class NewUserMessage
+{
+    private User $user;
+
+    private Hospital $hospital;
+
+    public function __construct(User $user, Hospital $hospital)
+    {
+        $this->user = $user;
+        $this->hospital = $hospital;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function getHospital(): Hospital
+    {
+        return $this->hospital;
+    }
+}
