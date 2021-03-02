972ec971c0f76666bb8af215af93f368cd29b134
Christoph Steltner <mail@nplhse.com>
2021-03-02 17:11:52 +0100

    feat: Made confirmation emails work.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,13 +3,24 @@
 namespace App\Controller;
 
 use App\Form\SecurityLoginType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 
 class SecurityController extends AbstractController
 {
+    private VerifyEmailHelperInterface $verifyEmailHelper;
+
+    public function __construct(VerifyEmailHelperInterface $verifyEmailHelper)
+    {
+        $this->verifyEmailHelper = $verifyEmailHelper;
+    }
+
     /**
      * @Route("/login", name="app_login")
      */
@@ -43,5 +54,47 @@ class SecurityController extends AbstractController
     public function logout(): Response
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/verify", name="app_confirm_email")
+     */
+    public function verifyUserEmail(Request $request, UserRepository $userRepository): Response
+    {
+        $user = $this->getUser();
+
+        if (null === $user) {
+            $id = $request->get('id'); // retrieve the user id from the url
+
+            // Verify the user id exists and is not null
+            if (null === $id) {
+                return $this->redirectToRoute('default');
+            }
+
+            $user = $userRepository->findOneBy(['id' => $id]);
+
+            // Ensure the user exists in persistence
+            if (null === $user) {
+                return $this->redirectToRoute('default');
+            }
+        }
+
+        try {
+            $this->verifyEmailHelper->validateEmailConfirmation($request->getUri(), $user->getId(), $user->getEmail());
+
+            $user->setIsVerified(true);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+        } catch (VerifyEmailExceptionInterface $e) {
+            $this->addFlash('danger', 'Verification failed.');
+
+            return $this->redirectToRoute('default');
+        }
+
+        $this->addFlash('success', 'Your E-Mail address has been verified!');
+
+        return $this->redirectToRoute('default');
     }
 }
--- src/Controller/User/AccountEmailController.php
+++ src/Controller/User/AccountEmailController.php
@@ -4,10 +4,15 @@ namespace App\Controller\User;
 
 use App\Form\EmailChangeType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\ExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 
 /**
  * @Route("/user/email")
@@ -15,6 +20,16 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class AccountEmailController extends AbstractController
 {
+    private MailerInterface $mailer;
+
+    private VerifyEmailHelperInterface $verifyEmailHelper;
+
+    public function __construct(MailerInterface $mailer, VerifyEmailHelperInterface $verifyEmailHelper)
+    {
+        $this->mailer = $mailer;
+        $this->verifyEmailHelper = $verifyEmailHelper;
+    }
+
     /**
      * @Route("/", name="account_email")
      */
@@ -37,6 +52,29 @@ class AccountEmailController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
+            $signatureComponents = $this->verifyEmailHelper->generateSignature(
+                'app_confirm_email',
+                $user->getId(),
+                $user->getEmail()
+            );
+
+            try {
+                $email = (new TemplatedEmail())
+                    ->to(new Address($user->getEmail()))
+                    ->subject('Please verify your new E-Mail address')
+                    ->htmlTemplate('user/emails/verify_email.html.twig')
+                    ->context([
+                        'signedUrl' => $signatureComponents->getSignedUrl(),
+                        'expiration' => '3600',
+                    ]);
+
+                $this->mailer->send($email);
+            } catch (ExceptionInterface $e) {
+                $this->addFlash('warning', 'Your E-Mail address has been updated. But the system failed to send you an confirmation message.');
+
+                return $this->redirectToRoute('account_email');
+            }
+
             $this->addFlash('success', 'Your E-Mail address has been updated. Also a confirmation has been sent to your E-Mail address.');
 
             return $this->redirectToRoute('account_email');
@@ -47,5 +85,47 @@ class AccountEmailController extends AbstractController
             'form_email' => $form->createView(),
             'errors' => $form->getErrors(true, false),
         ]);
+    }
+
+    /**
+     * @Route("/verify", name="account_email_verify")
+     */
+    public function sendVerification(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
+
+        if ($user->getIsVerified()) {
+            $this->addFlash('warning', 'Your E-Mail address is already verified.');
+
+            return $this->redirectToRoute('account_email');
+        }
+
+        $signatureComponents = $this->verifyEmailHelper->generateSignature(
+            'app_confirm_email',
+            $user->getId(),
+            $user->getEmail()
+        );
+
+        try {
+            $email = (new TemplatedEmail())
+                ->to(new Address($user->getEmail()))
+                ->subject('Please verify your new E-Mail address')
+                ->htmlTemplate('user/emails/verify_email.html.twig')
+                ->context([
+                    'signedUrl' => $signatureComponents->getSignedUrl(),
+                    'expiration' => '3600',
+                ]);
+
+            $this->mailer->send($email);
+        } catch (ExceptionInterface $e) {
+            $this->addFlash('danger', 'Cannot send you an new confirmation E-Mail.');
+
+            return $this->redirectToRoute('account_email');
+        }
+
+        $this->addFlash('success', 'A new confirmation has been sent to your E-Mail address.');
+
+        return $this->redirectToRoute('account_email');
     }
 }
