06cc9733229ad6e2f3c92f64d106315a62a803ff
Christoph Steltner <mail@nplhse.com>
2021-04-19 21:29:15 +0200

    feat: Added statistics controller

---
+++ src/Controller/StatisticsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StatisticsController extends AbstractController
+{
+    #[Route('/statistics', name: 'statistics_index')]
+    public function index(): Response
+    {
+        return $this->render('statistics/index.html.twig', [
+            'controller_name' => 'StatisticsController',
+        ]);
+    }
+}
