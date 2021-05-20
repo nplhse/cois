17be27b8cbcbce7485f5d389867d53a4b2483435
nplhse <mail@nplhse.com>
2021-05-20 20:17:00 +0200

    feat: Added translations to allocations

--- src/Controller/AllocationController.php
+++ src/Controller/AllocationController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/allocations")
+ * @Route("/{_locale<%app.supported_locales%>}/allocations")
  * @IsGranted("ROLE_USER")
  */
 class AllocationController extends AbstractController
