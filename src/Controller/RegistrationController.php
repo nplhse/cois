9bebd11e38c577ec17542c47451fed28ae4fde7a
Christoph Steltner <mail@nplhse.com>
2021-06-17 10:04:30 +0200

    fix: Small adjustments

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -55,7 +55,7 @@ class RegistrationController extends AbstractController
                     ->from(new Address('noreply@example.com', 'Collaborative IVENA statistics'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
+                    ->htmlTemplate('registration/emails/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
 
