d3cb2180208f55442778d2cc850742bb57c14b1c
Christoph Steltner <mail@nplhse.com>
2021-10-02 21:19:26 +0200

    feat: Added preview for new Allocation table

--- src/Controller/AllocationController.php
+++ src/Controller/AllocationController.php
@@ -3,21 +3,22 @@
 namespace App\Controller;
 
 use App\Entity\Allocation;
+use App\Repository\AllocationRepository;
 use App\Repository\HospitalRepository;
 use App\Repository\ImportRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/{_locale<%app.supported_locales%>}/allocations")
  * @IsGranted("ROLE_USER")
  */
 class AllocationController extends AbstractController
 {
     /**
-     * @Route("/", name="allocation_index")
+     * @Route("/{_locale<%app.supported_locales%>}/allocations/", name="allocation_index")
      */
     public function index(HospitalRepository $hospitalRepository): Response
     {
@@ -37,8 +38,52 @@ class AllocationController extends AbstractController
         ]);
     }
 
+    #[Route('/_new', name: 'app_allocation_index', methods: ['GET'])]
+    public function index_new(Request $request, AllocationRepository $allocationRepository, HospitalRepository $hospitalRepository): Response
+    {
+        $hospital = $request->query->get('hospital');
+        $supplyArea = $request->query->get('supplyArea');
+        $dispatchArea = $request->query->get('dispatchArea');
+
+        $filters = [];
+        $filters['search'] = $request->query->get('search');
+
+        if ($hospital) {
+            $filters['hospital'] = $hospital;
+        } else {
+            $filters['hospital'] = null;
+        }
+
+        if ($supplyArea) {
+            $filters['supplyArea'] = $supplyArea;
+        } else {
+            $filters['supplyArea'] = null;
+        }
+
+        if ($dispatchArea) {
+            $filters['dispatchArea'] = $dispatchArea;
+        } else {
+            $filters['dispatchArea'] = null;
+        }
+
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $allocationRepository->getAllocationPaginator($offset, $filters);
+
+        return $this->render('allocation/index_new.html.twig', [
+            'allocations' => $paginator,
+            'search' => $filters['search'],
+            'filters' => $filters,
+            'perPage' => AllocationRepository::PAGINATOR_PER_PAGE,
+            'previous' => $offset - AllocationRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + AllocationRepository::PAGINATOR_PER_PAGE),
+            'hospitals' => $hospitalRepository->getHospitals(),
+            'supplyAreas' => $hospitalRepository->getSupplyAreas(),
+            'dispatchAreas' => $hospitalRepository->getDispatchAreas(),
+        ]);
+    }
+
     /**
-     * @Route("/{id}", name="allocation_show", methods={"GET"})
+     * @Route("/{_locale<%app.supported_locales%>}/allocations/{id}", name="allocation_show")
      */
     public function show(Allocation $allocation, ImportRepository $importRepository): Response
     {
