bb858970816c6281679d581823d5b6fab199487e
Christoph Steltner <mail@nplhse.com>
2021-10-28 17:28:37 +0200

    feat: Finally added PZCs

---
+++ assets/controllers/table_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+    static targets = ["source", "filterable"];
+
+    filter(event) {
+        let lowerCaseFilterTerm = this.sourceTarget.value.toLowerCase();
+
+        this.filterableTargets.forEach((el, i) => {
+            let filterableKey = el
+                .getAttribute("data-filter-key")
+                .toLowerCase();
+
+            el.classList.toggle(
+                "filter--notFound",
+                !filterableKey.includes(lowerCaseFilterTerm)
+            );
+        });
+    }
+}
