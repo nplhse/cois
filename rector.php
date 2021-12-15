b13646cf8666ab04cce20fe7e5d95120c8434355
Christoph Steltner <mail@nplhse.com>
2021-12-15 13:41:12 +0100

    Added rector

---
+++ rector.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    // get parameters
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, getcwd().'/phpstan.neon.dist');
+
+    $parameters->set(Option::PATHS, [
+        __DIR__.'/src',
+    ]);
+
+    // region Symfony Container
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(
+        Option::SYMFONY_CONTAINER_XML_PATH_PARAMETER,
+        __DIR__.'/var/cache/dev/App_KernelDevDebugContainer.xml'
+    );
+    // endregion
+
+    // Define what rule sets will be applied
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_54);
+    $containerConfigurator->import(SymfonySetList::SYMFONY_CODE_QUALITY);
+
+    // get services (needed for register a single rule)
+    // $services = $containerConfigurator->services();
+
+    // register a single rule
+    // $services->set(TypedPropertyRector::class);
+};
