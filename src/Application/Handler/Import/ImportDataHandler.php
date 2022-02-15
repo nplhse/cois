f1f5a97d8f189e417cff84c7ed432071e2d8f0e6
Christoph Steltner <mail@nplhse.com>
2022-02-15 23:15:46 +0100

    fix: Working around legacy data

--- src/Application/Handler/Import/ImportDataHandler.php
+++ src/Application/Handler/Import/ImportDataHandler.php
@@ -3,17 +3,12 @@
 namespace App\Application\Handler\Import;
 
 use App\Application\Contract\HandlerInterface;
-use App\Application\Exception\ImportMimeTypeNotSupportedException;
 use App\Application\Exception\ImportNotFoundException;
-use App\Application\Exception\ImportTypeNotSupported;
 use App\Domain\Command\Import\ImportDataCommand;
-use App\Domain\Contracts\ImportInterface;
 use App\Domain\Event\Import\ImportSuccessEvent;
 use App\Domain\Repository\ImportRepositoryInterface;
-use App\Entity\Import;
 use App\Service\ImportService;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\Filesystem\Filesystem;
 
 class ImportDataHandler implements HandlerInterface
 {
@@ -44,38 +39,19 @@ class ImportDataHandler implements HandlerInterface
             throw new ImportNotFoundException('Could not find Import with id %d', $command->getImportId());
         }
 
-        $path = $this->projectDir.self::Import_DIR.$import->getFilePath();
+        if (null === $import->getFilePath()) {
+            $path = $this->projectDir.self::Import_DIR.$import->getPath();
+            $result = $this->importService->import($path, $import->getMimeType());
+        } elseif ('' === $import->getFilePath()) {
+            $path = $this->projectDir.self::Import_DIR.$import->getPath();
+            $result = $this->importService->import($path, $import->getMimeType());
+        } else {
+            $path = $this->projectDir.self::Import_DIR.$import->getFilePath();
+            $result = $this->importService->import($path, $import->getFileMimeType());
+        }
 
-        $result = $this->importService->import($path, $import->getFileMimeType());
         $this->importService->process($result, $import);
 
-        //$result = $this->loadData($import);
-        // $this->processData($result, $import);
-
         $this->dispatcher->dispatch(new ImportSuccessEvent($import), ImportSuccessEvent::NAME);
-    }
-
-    private function loadData(ImportInterface $import): iterable
-    {
-        $filesystem = new Filesystem();
-
-        $path = $this->projectDir.self::Import_DIR.$import->getFilePath();
-        $result = match ($import->getFileMimeType()) {
-            \App\Domain\Entity\Import::MIME_CSV, \App\Domain\Entity\Import::MIME_PLAIN => $this->importService->import($path, $import->getFileMimeType()),
-            default => throw new ImportMimeTypeNotSupportedException(sprintf('MimeType %s of Import %d is not supported.', $import->getFileMimeType(), $import->getId())),
-        };
-
-        return $result;
-    }
-
-    private function processData(iterable $result, ImportInterface $import): void
-    {
-        switch ($import->getType()) {
-            case Import::TYPE_ALLOCATION:
-                 $this->importService->processToAllocation($result, $import);
-                break;
-            default:
-                throw new ImportTypeNotSupported(sprintf('Import type %s of Import %d is not supported.', $import->getType(), $import->getId()));
-        }
     }
 }
