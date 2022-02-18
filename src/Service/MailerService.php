50f6b74f947e659c54863b7b222b4ef9599f0c00
Christoph Steltner <mail@nplhse.com>
2022-02-18 19:26:00 +0100

    refactoring: Minor smaller issues

--- src/Service/MailerService.php
+++ src/Service/MailerService.php
@@ -8,6 +8,7 @@ use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Generator\UrlGenerator;
 use Symfony\Component\Routing\RouterInterface;
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordToken;
 
 class MailerService
 {
@@ -37,6 +38,36 @@ class MailerService
             ->context([
                 'user' => $user,
                 'link' => $this->router->generate('app_import_new', [], UrlGenerator::ABSOLUTE_URL),
+            ]);
+
+        $this->mailer->send($email);
+    }
+
+    public function sendPasswordResetEmail(User $user, ResetPasswordToken $resetToken): void
+    {
+        $email = (new TemplatedEmail())
+            ->to(new Address($user->getEmail()))
+            ->from(new Address($this->mailerSender, $this->mailerFrom))
+            ->subject('Reset your Password')
+            ->htmlTemplate('emails/user/reset_password.inky.twig')
+            ->context([
+                'resetToken' => $resetToken,
+            ]);
+
+        $this->mailer->send($email);
+    }
+
+    public function sendVerificationEmail(User $user, string $signedUrl, int $expiration): void
+    {
+        $email = (new TemplatedEmail())
+            ->to(new Address($user->getEmail()))
+            ->from(new Address($this->mailerSender, $this->mailerFrom))
+            ->subject('Verify your E-Mail address')
+            ->htmlTemplate('emails/user/verify_email.inky.twig')
+            ->context([
+                'user' => $user,
+                'signedUrl' => $signedUrl,
+                'expiration' => '3600',
             ]);
 
         $this->mailer->send($email);
