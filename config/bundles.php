cf46b2cf0a8cc84731cba602577f55070b0adebf
nplhse <mail@nplhse.com>
2022-03-16 14:20:17 +0100

    feat: Added doctrine-test-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
