cc1c48bbc2f80f96b2b421e72b2e14b18be8f97a
Christoph Steltner <mail@nplhse.com>
2022-03-16 22:28:50 +0100

    fix: Added 50 chars to handoverPoint
    Thank you Marburg...

--- src/Entity/Allocation.php
+++ src/Entity/Allocation.php
@@ -271,7 +271,7 @@ class Allocation extends DomainAllocation
     protected bool $specialityWasClosed;
 
     /**
-     * @ORM\Column(type="string", length=50, nullable=true)
+     * @ORM\Column(type="string", length=100, nullable=true)
      */
     protected ?string $handoverPoint = null;
 
