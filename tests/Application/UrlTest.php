567a3ec241cbaa1d52bb81ff8ae91689a60e0f3d
Christoph Steltner <mail@nplhse.com>
2022-03-17 15:18:23 +0100

    test: Added Test for public/secure Urls

---
+++ tests/Application/UrlTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Tests\Application;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class UrlTest extends WebTestCase
+{
+    /**
+     * @dataProvider getPublicUrls
+     */
+    public function testPublicUrls(string $url): void
+    {
+        $client = static::createClient();
+        $client->request('GET', $url);
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    /**
+     * @dataProvider getSecureUrls
+     */
+    public function testSecureUrls(string $url): void
+    {
+        $client = static::createClient();
+        $client->request('GET', $url);
+
+        $this->assertResponseRedirects(
+            '/login',
+            Response::HTTP_FOUND
+        );
+    }
+
+    public function getPublicUrls(): ?\Generator
+    {
+        yield ['/'];
+        yield ['/login'];
+        yield ['reset-password/'];
+    }
+
+    public function getSecureUrls(): ?\Generator
+    {
+        yield ['/dashboard/'];
+        yield ['/settings'];
+        yield ['/settings/profile'];
+        yield ['/settings/email'];
+        yield ['/settings/password'];
+        yield ['/hospitals/'];
+        yield ['/hospitals/new'];
+        yield ['/allocations/'];
+        yield ['/import/'];
+    }
+}
