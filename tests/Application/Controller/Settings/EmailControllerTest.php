b6e5a5d10d4e9275674d105df0653cf38b1f2366
Christoph Steltner <mail@nplhse.com>
2022-03-18 20:18:20 +0100

    test: Added EmailControllerTest

---
+++ tests/Application/Controller/Settings/EmailControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Tests\Application\Controller\Settings;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Zenstruck\Browser\Test\HasBrowser;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+class EmailControllerTest extends WebTestCase
+{
+    use HasBrowser;
+    use ResetDatabase;
+
+    public function testChangeEmail(): void
+    {
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneBy(['username' => 'foo']);
+
+        $this->browser()
+            ->actingAs($testUser)
+            ->visit('/settings/email')
+            ->assertSee('E-Mail settings')
+            ->assertSee('Your E-Mail address is currently: verified.')
+            ->fillField('New email', 'testEmail@example.com')
+            ->click('Update E-Mail settings')
+            ->assertSuccessful()
+            ->assertSee('Your current E-Mail address is: testEmail@example.com')
+            ->assertSee('Your E-Mail address is currently: not verified.')
+        ;
+    }
+}
