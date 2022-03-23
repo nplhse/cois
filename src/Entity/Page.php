b4f36750d79c87eccb2241c0313bc7ea08ef5a83
Christoph Steltner <mail@nplhse.com>
2022-03-23 14:24:54 +0100

    feat: Created Page entity and Blamable trait

---
+++ src/Entity/Page.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Entity;
+
+use App\Domain\Contracts\UserInterface;
+use App\Domain\Entity\Traits\BlamableTrait;
+use App\Domain\Entity\Traits\TimestampableTrait;
+use App\Repository\PageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PageRepository::class)
+ */
+class Page
+{
+    use BlamableTrait;
+    use TimestampableTrait;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private string $title;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private string $slug;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private string $type;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private string $content;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private string $status;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected UserInterface $createdBy;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected ?UserInterface $updatedBy = null;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected \DateTimeInterface $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected ?\DateTimeInterface $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
