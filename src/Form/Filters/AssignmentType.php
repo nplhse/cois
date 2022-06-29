2d8a424c821da3454f7365621b4a0b72948164b3
nplhse <mail@nplhse.com>
2022-06-29 17:14:23 +0200

    feat: Added caching to forms

--- src/Form/Filters/AssignmentType.php
+++ src/Form/Filters/AssignmentType.php
@@ -6,21 +6,26 @@ use App\Repository\AllocationRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class AssignmentType extends AbstractType
 {
+    private TagAwareCacheInterface $cache;
+
     private AllocationRepository $allocationRepository;
 
-    public function __construct(AllocationRepository $allocationRepository)
+    public function __construct(AllocationRepository $allocationRepository, TagAwareCacheInterface $appCache)
     {
         $this->allocationRepository = $allocationRepository;
+        $this->cache = $appCache;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'required' => false,
-            'choices' => $this->allocationRepository->getAssignmentsArray(),
+            'choices' => $this->getAssignments(),
             'placeholder' => 'All assignments',
             'empty_data' => '',
         ]);
@@ -29,5 +34,15 @@ class AssignmentType extends AbstractType
     public function getParent(): string
     {
         return ChoiceType::class;
+    }
+
+    private function getAssignments(): array
+    {
+        return $this->cache->get('form-assignment', function (ItemInterface $item) {
+            $item->expiresAfter(3600);
+            $item->tag(['form', 'form-assignment']);
+
+            return $this->allocationRepository->getAssignmentsArray();
+        });
     }
 }
