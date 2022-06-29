7ac65632d7cfa49bb9f91f1c4b4259a31d8ac3e5
Christoph Steltner <mail@nplhse.com>
2022-06-29 17:33:12 +0200

    Add more caching (#1054)
    * feat: Added caching to forms
    
    * feat: Added command to clear app cache

--- src/Form/Filters/IndicationType.php
+++ src/Form/Filters/IndicationType.php
@@ -6,21 +6,26 @@ use App\Repository\AllocationRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class IndicationType extends AbstractType
 {
+    private TagAwareCacheInterface $cache;
+
     private AllocationRepository $allocationRepository;
 
-    public function __construct(AllocationRepository $allocationRepository)
+    public function __construct(AllocationRepository $allocationRepository, TagAwareCacheInterface $appCache)
     {
         $this->allocationRepository = $allocationRepository;
+        $this->cache = $appCache;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'required' => false,
-            'choices' => array_flip($this->allocationRepository->getIndicationsArray()),
+            'choices' => array_flip($this->getIndications()),
             'placeholder' => 'All indications',
             'empty_data' => '',
         ]);
@@ -29,5 +34,15 @@ class IndicationType extends AbstractType
     public function getParent(): string
     {
         return ChoiceType::class;
+    }
+
+    private function getIndications(): array
+    {
+        return $this->cache->get('form-indications', function (ItemInterface $item) {
+            $item->expiresAfter(3600);
+            $item->tag(['form', 'form-indications']);
+
+            return $this->allocationRepository->getIndicationsArray();
+        });
     }
 }
--- src/Form/Filters/SpecialityDetailType.php
+++ src/Form/Filters/SpecialityDetailType.php
@@ -6,21 +6,26 @@ use App\Repository\AllocationRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class SpecialityDetailType extends AbstractType
 {
+    private TagAwareCacheInterface $cache;
+
     private AllocationRepository $allocationRepository;
 
-    public function __construct(AllocationRepository $allocationRepository)
+    public function __construct(AllocationRepository $allocationRepository, TagAwareCacheInterface $appCache)
     {
         $this->allocationRepository = $allocationRepository;
+        $this->cache = $appCache;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'required' => false,
-            'choices' => array_flip($this->allocationRepository->getSpecialityDetailArray()),
+            'choices' => array_flip($this->getSpecialityDetails()),
             'placeholder' => 'All speciality details',
             'empty_data' => '',
         ]);
@@ -29,5 +34,15 @@ class SpecialityDetailType extends AbstractType
     public function getParent(): string
     {
         return ChoiceType::class;
+    }
+
+    private function getSpecialityDetails(): array
+    {
+        return $this->cache->get('form-speciality-details', function (ItemInterface $item) {
+            $item->expiresAfter(3600);
+            $item->tag(['form', 'form-speciality-details']);
+
+            return $this->allocationRepository->getOccasionsArray();
+        });
     }
 }
