73f9ba3954e466f4e825cfc017b1f7a7b9d5feac
nplhse <mail@nplhse.com>
2022-06-29 17:26:01 +0200

    feat: Added command to clear app cache

---
+++ src/Command/ClearFormCacheCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
+
+#[AsCommand(
+    name: 'app:clear-cache',
+    description: 'Clear application related caching',
+)]
+class ClearFormCacheCommand extends Command
+{
+    private TagAwareCacheInterface $cache;
+
+    public function __construct(TagAwareCacheInterface $appCache)
+    {
+        parent::__construct();
+
+        $this->cache = $appCache;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('type', InputArgument::REQUIRED, 'Set the type of cache to be cleared.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $type = $input->getArgument('type');
+
+        $this->cache->invalidateTags([$type]);
+
+        $io->success('Successfully cleared cache: '.$type);
+
+        return Command::SUCCESS;
+    }
+}
