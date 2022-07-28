221ddcc7d554c128c70df3e4e1587b16213736e3
Christoph Steltner <mail@nplhse.com>
2022-07-28 13:55:12 +0200

    Still fixing emails (#1106)
    * fix: Simplified Webpack config
    
    * feat: Added custom extension to read assets
    
    * style: Forgot some types

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
     .addEntry("app", "./assets/js/app.js")
+    .addStyleEntry("emails", "./assets/styles/emails.scss")
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge("./assets/controllers.json")
@@ -42,7 +43,6 @@ Encore
     .cleanupOutputBeforeBuild(["public"], (options) => {
         options.verbose = true;
         options.root = __dirname;
-        options.exclude = ["emails"];
     })
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
@@ -99,52 +99,5 @@ Encore
         },
     ]);
 
-// build the first configuration
-const baseConfig = Encore.getWebpackConfig();
-
-// Set a unique name for the config (needed later!)
-baseConfig.name = "baseConfig";
-
-// reset Encore to build the second config
-Encore.reset();
-
-Encore
-    // directory where compiled assets will be stored
-    .setOutputPath("public/build/emails")
-    // public path used by the web server to access the output path
-    .setPublicPath("/build")
-    // only needed for CDN's or sub-directory deploy
-    // .setManifestKeyPrefix('build/')
-
-    /*
-     * ENTRY CONFIG
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
-     */
-    .addStyleEntry("emails", "./assets/styles/emails.scss")
-
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
-
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
-    .enableBuildNotifications()
-
-    // enables Sass/SCSS support
-    .enableSassLoader();
-
-// build the second configuration
-const mailerConfig = Encore.getWebpackConfig();
-
-// Set a unique name for the config (needed later!)
-mailerConfig.name = "mailerConfig";
-
 // export the final configuration as an array of multiple configurations
-module.exports = [baseConfig, mailerConfig];
+module.exports = Encore.getWebpackConfig();
