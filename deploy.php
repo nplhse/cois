dfe7c6a02077376e96202924b4bf52b056ab9f1b
Christoph Steltner <mail@nplhse.com>
2022-11-21 16:45:05 +0100

    Upgrade deployer (#1255)
    * build(deps): Updated to deployer 7.0.2
    
    * build(composer): Added assets:install to auto-scripts
    
    * build: Upgraded and improved deployer.php

--- deploy.php
+++ deploy.php
@@ -3,49 +3,57 @@
 namespace Deployer;
 
 require 'recipe/symfony.php';
+require 'contrib/webpack_encore.php';
 
-// Config
+// Base configuration
 set('repository', 'https://github.com/nplhse/cois.git');
-set('keep_releases', 4);
+set('keep_releases', 3);
 set('writable_mode', 'chmod');
 set('env', [
     'APP_ENV' => 'prod',
 ]);
 
-// Directories
+// Set directories for symfony
 add('shared_dirs', [
     'var/log',
     'var/storage',
     ]);
+
 add('writable_dirs', [
     'var',
     'var/cache',
     'var/log',
+    'var/sessions',
     'var/storage',
 ]);
 
-// Hosts
+/*
+ * For security reasons we're importing a local hosts.yaml file that includes
+ * hostnames etc. for our deployments.
+ * For details see: https://deployer.org/docs/7.x/hosts#yaml-inventory
+ */
 import('hosts.yaml');
+
+/*
+ * Because deployment might fail when installing the yarn dependencies and the
+ * building all the webpack encore stuff we're using a customized task that is
+ * performing a local build followed by an upload of the "/public/build"
+ * directory. If you don't need this special setup you could replace all of
+ * this by the following code:
+ *
+ *      after('deploy:vendors', 'yarn:install');
+ *      after('deploy:vendors', 'webpack_encore:build');
+ */
 
 // Customized tasks
 task('build_locally', function () {
     runLocally('yarn install');
     runLocally('yarn build');
-    upload('./public/build', '{{release_path}}/public/.');
-});
-
-task('build_remote', function () {
-    cd('{{release_path}}');
-    run('yarn install');
-    run('yarn build');
+    upload('./public/', '{{release_path}}/public/.');
 });
 
 // Attach custom tasks to default workflow
-before('deploy:symlink', 'database:migrate');
-
-// Switch "build_locally" with "build_remote" if your setup requires
 after('deploy:vendors', 'build_locally');
 
-// On failure
-after('deploy:failed', 'deploy:unlock');
-after('deploy:failed', 'rollback');
+// Attach tasks from recipes& contrib to default workflow
+before('deploy:symlink', 'database:migrate');
