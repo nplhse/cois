3578a477b6766436ea1135997f49abdbc3708a8e
Christoph Steltner <mail@nplhse.com>
2023-01-01 14:27:02 +0100

    feat: Updating doctrine config

--- config/packages/doctrine.php
+++ config/packages/doctrine.php
@@ -4,28 +4,26 @@ declare(strict_types=1);
 
 use DoctrineExtensions\Query\Mysql\DateFormat;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Config\DoctrineConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension('doctrine', [
-        'dbal' => [
-            'url' => '%env(resolve:DATABASE_URL)%',
-        ],
-        'orm' => [
-            'auto_generate_proxy_classes' => true,
-            'naming_strategy' => 'doctrine.orm.naming_strategy.underscore_number_aware',
-            'auto_mapping' => true,
-            'mappings' => [
-                'App' => [
-                    'is_bundle' => false,
-                    'type' => 'attribute',
-                    'dir' => '%kernel.project_dir%/src/Entity',
-                    'prefix' => 'App\Entity', 'alias' => 'App',
-                ],
-            ],
-            'dql' => [
-                'datetime_functions' => [
-                    'DATE_FORMAT' => DateFormat::class, ],
-            ],
+return static function (DoctrineConfig $doctrineConfig, ContainerConfigurator $containerConfigurator): void {
+    $doctrineConfig->dbal()->connection('default')->url('%env(resolve:DATABASE_URL)%');
+
+    $emDefault = $doctrineConfig->orm()->entityManager('default');
+
+    $emDefault->namingStrategy('doctrine.orm.naming_strategy.underscore_number_aware');
+    $emDefault->autoMapping(true);
+
+    $emDefault->mapping('App')
+        ->isBundle(false)
+        ->type('attribute')
+        ->dir('%kernel.project_dir%/src/Entity')
+        ->prefix('App\Entity')
+        ->alias('App');
+
+    $emDefault->dql([
+        'datetime_functions' => [
+            'DATE_FORMAT' => DateFormat::class,
         ],
     ]);
 };
