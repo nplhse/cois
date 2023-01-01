17c50c3ca45eff386155459ec72803ff70c8f6a8
Christoph Steltner <mail@nplhse.com>
2023-01-01 14:34:55 +0100

    style: Updated config file style issue

--- config/packages/doctrine_migrations.php
+++ config/packages/doctrine_migrations.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-
-return static function (\Symfony\Config\DoctrineMigrationsConfig $migrationsConfig): void {
+return static function (Symfony\Config\DoctrineMigrationsConfig $migrationsConfig): void {
     $migrationsConfig->migrationsPath('DoctrineMigrations', '%kernel.project_dir%/migrations');
     $migrationsConfig->enableProfiler(false);
 };
