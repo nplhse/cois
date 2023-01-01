ebb7e2aa093b245fde355a106e823f57faac0f35
Christoph Steltner <mail@nplhse.com>
2023-01-01 15:53:49 +0100

    build: Some other updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var Encore = require("@symfony/webpack-encore");
+const Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -10,9 +10,9 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath("public/build/")
     // public path used by the web server to access the output path
-    .setPublicPath("/build")
-    // only needed for CDN's or sub-directory deploy
-    // .setManifestKeyPrefix('build/')
+    .setPublicPath('/build')
+    // only needed for CDN's or subdirectory deploy
+    //.setManifestKeyPrefix('build/')
 
     /*
      * ENTRY CONFIG
@@ -50,10 +50,10 @@ Encore
         config.plugins.push("@babel/plugin-proposal-class-properties");
     })
 
-    // enables @babel/preset-env polyfills
+    // enables and configure @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = "usage";
-        config.corejs = 3;
+        config.useBuiltIns = 'usage';
+        config.corejs = '3.23';
     })
 
     // enables Sass/SCSS support
