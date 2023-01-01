28f988eed469225e506e923344b1b8a7ea6293a8
Christoph Steltner <mail@nplhse.com>
2023-01-01 14:46:53 +0100

    build: Updated flysystem recipe

--- config/packages/flysystem.php
+++ config/packages/flysystem.php
@@ -2,17 +2,10 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension('flysystem', [
-        'storages' => [
-            'default.storage' => [
-                'adapter' => 'local',
-                'options' => [
-                    'directory' => '%kernel.project_dir%/var/storage/import',
-                ],
-            ],
-        ],
-    ]);
+return static function (Symfony\Config\FlysystemConfig $flysystemConfig): void {
+    $flysystemConfig->storage('defaultStorage')
+        ->adapter('local')
+        ->options([
+            'directory' => '%kernel.project_dir%/var/storage/import',
+        ]);
 };
