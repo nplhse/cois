d5758e731a1193dd5c01518a3b2d1d805423e46f
Christoph Steltner <mail@nplhse.com>
2023-01-02 11:51:04 +0100

    Performed some tidy up in all configs

--- config/packages/doctrine_migrations.php
+++ config/packages/doctrine_migrations.php
@@ -2,7 +2,13 @@
 
 declare(strict_types=1);
 
-return static function (Symfony\Config\DoctrineMigrationsConfig $migrationsConfig): void {
-    $migrationsConfig->migrationsPath('DoctrineMigrations', '%kernel.project_dir%/migrations');
-    $migrationsConfig->enableProfiler(false);
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->extension('doctrine_migrations', [
+        'migrations_paths' => [
+            'DoctrineMigrations' => '%kernel.project_dir%/migrations',
+        ],
+        'enable_profiler' => false,
+    ]);
 };
--- config/packages/framework.php
+++ config/packages/framework.php
@@ -6,12 +6,17 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->extension('framework', [
-        'secret' => '%env(APP_SECRET)%',
+        'assets' => [
+            'json_manifest_path' => '%kernel.project_dir%/public/build/manifest.json',
+        ],
         'csrf_protection' => true,
+        'secret' => '%env(APP_SECRET)%',
+        'http_method_override' => false,
         'session' => [
             'handler_id' => null,
             'cookie_secure' => 'auto',
             'cookie_samesite' => 'lax',
+            'storage_factory_id' => 'session.storage.factory.native',
         ],
         'php_errors' => [
             'log' => true,
