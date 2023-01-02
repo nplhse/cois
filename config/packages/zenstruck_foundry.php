1a93fedcfb7e7541105090c67f56af93fc6557b1
Christoph Steltner <mail@nplhse.com>
2023-01-02 11:50:28 +0100

    Updated zenstruck/foundry config

--- config/packages/zenstruck_foundry.php
+++ config/packages/zenstruck_foundry.php
@@ -5,14 +5,16 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    if ('dev' === $containerConfigurator->env()) {
-        $containerConfigurator->extension('zenstruck_foundry', [
-            'auto_refresh_proxies' => true,
-        ]);
-    }
+    $containerConfigurator->extension('zenstruck_foundry', [
+        // Whether to auto-refresh proxies by default (https://github.com/zenstruck/foundry#auto-refresh)
+        'auto_refresh_proxies' => false,
+    ]);
+
     if ('test' === $containerConfigurator->env()) {
         $containerConfigurator->extension('zenstruck_foundry', [
-            'auto_refresh_proxies' => true,
+            'global_state' => [
+                'App\Tests\Story\GlobalStory',
+            ],
         ]);
     }
 };
