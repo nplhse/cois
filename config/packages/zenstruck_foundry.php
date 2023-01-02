6d20426a6a97155f76873f58bcd384b3d1584b41
Christoph Steltner <mail@nplhse.com>
2023-01-02 12:24:15 +0100

    fix: Focussing config on environments

--- config/packages/zenstruck_foundry.php
+++ config/packages/zenstruck_foundry.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension('zenstruck_foundry', [
-        // Whether to auto-refresh proxies by default (https://github.com/zenstruck/foundry#auto-refresh)
-        'auto_refresh_proxies' => false,
-    ]);
+    if ('dev' === $containerConfigurator->env()) {
+        $containerConfigurator->extension('zenstruck_foundry', [
+            // Whether to auto-refresh proxies by default (https://github.com/zenstruck/foundry#auto-refresh)
+            'auto_refresh_proxies' => false,
+        ]);
+    }
 
     if ('test' === $containerConfigurator->env()) {
         $containerConfigurator->extension('zenstruck_foundry', [
+            // Whether to auto-refresh proxies by default (https://github.com/zenstruck/foundry#auto-refresh)
+            'auto_refresh_proxies' => false,
             'global_state' => [
                 'App\Tests\Story\GlobalStory',
             ],
