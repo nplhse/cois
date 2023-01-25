2bdb58439c482a261d0262b724e20d61886e44e6
Christoph Steltner <mail@nplhse.com>
2023-01-25 13:12:24 +0100

    feat: Moved special pages

---
+++ src/Controller/Website/TermsController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Website;
+
+use App\Repository\PageRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TermsController extends AbstractController
+{
+    public function __construct(
+        private PageRepository $pageRepository
+    ) {
+    }
+
+    #[Route(path: [
+        'en' => '/terms',
+        'de' => '/nutzungsbedingungen',
+    ], name: 'app_page_terms')]
+    public function index(): Response
+    {
+        $page = $this->pageRepository->findOneBy(['slug' => 'terms']);
+
+        if (null === $page) {
+            throw $this->createNotFoundException('Page could not be found');
+        }
+
+        return $this->render('website/page/terms.html.twig', [
+            'page' => $page,
+        ]);
+    }
+}
