33ef18a8fa5ccbf8c73983b2df46e595c2729c18
Christoph Steltner <mail@nplhse.com>
2023-02-24 12:33:40 +0100

    feat: Added new base controller

---
+++ src/Controller/Data/Export/ExportController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Data\Export;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExportController extends AbstractController
+{
+    #[Route('/export/', name: 'app_export_index')]
+    public function index(): Response
+    {
+        return $this->render('data/export/index.html.twig', [
+            'controller_name' => 'ExportController',
+        ]);
+    }
+}
