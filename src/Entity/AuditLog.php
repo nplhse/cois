f2ff35c1996f541e9051d58a6c4a274b5938146c
Christoph Steltner <mail@nplhse.com>
2023-03-01 14:28:55 +0100

    feat: Added AuditLog

---
+++ src/Entity/AuditLog.php
@@ -0,0 +1,142 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\AuditLogRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AuditLogRepository::class)]
+class AuditLog
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $entityType = null;
+
+    #[ORM\Column]
+    private ?int $entityId = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\ManyToOne]
+    private ?User $user = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $action = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $requestRoute = null;
+
+    #[ORM\Column]
+    private array $eventData = [];
+
+    #[ORM\Column(length: 255)]
+    private ?string $ipAddress = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEntityType(): ?string
+    {
+        return $this->entityType;
+    }
+
+    public function setEntityType(string $entityType): self
+    {
+        $this->entityType = $entityType;
+
+        return $this;
+    }
+
+    public function getEntityId(): ?int
+    {
+        return $this->entityId;
+    }
+
+    public function setEntityId(int $entityId): self
+    {
+        $this->entityId = $entityId;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getAction(): ?string
+    {
+        return $this->action;
+    }
+
+    public function setAction(string $action): self
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
+    public function getRequestRoute(): ?string
+    {
+        return $this->requestRoute;
+    }
+
+    public function setRequestRoute(?string $requestRoute): self
+    {
+        $this->requestRoute = $requestRoute;
+
+        return $this;
+    }
+
+    public function getEventData(): array
+    {
+        return $this->eventData;
+    }
+
+    public function setEventData(array $eventData): self
+    {
+        $this->eventData = $eventData;
+
+        return $this;
+    }
+
+    public function getIpAddress(): ?string
+    {
+        return $this->ipAddress;
+    }
+
+    public function setIpAddress(string $ipAddress): self
+    {
+        $this->ipAddress = $ipAddress;
+
+        return $this;
+    }
+}
